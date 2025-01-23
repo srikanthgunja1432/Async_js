857cfc5b077bcc1dcad4e34569f376805804f8e3
srikanthgunja1432 <22b8a105b6@cvr.ac.in>
2025-01-23 06:04:42 +0530

    Added New FIles

--- fetch_4.js
+++
@@ -1,7 +0,0 @@
-import fetch from 'node-fetch';
-
-
-fetch('https://api.github.com/users/Srikanth-gunja/repos')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(() => console.log("error"));
